d8a64ee5886611fd4fdf577bafc3b85e27ab7a37
juanvegatrd <juan.vega@trade.ec>
2024-09-12 14:55:54 -0300

    change logout endpoint

--- next/app/api/auth/logout/route.ts
+++ next/app/api/auth/logout/route.ts
@@ -3,10 +3,10 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
-    const { error } = await supabase.auth.signOut();
+    const data = await supabase.auth.signOut();
 
-    if (error) {
-      return NextResponse.json({ error: error.message }, { status: 400 });
+    if (data.error !== null) {
+      return NextResponse.json({ error: data.error.message }, { status: 400 });
     }
 
     return NextResponse.json({ message: "Logout successful" });
